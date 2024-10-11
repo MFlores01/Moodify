d2f27ca1a2d9d8f99016e5977948593505a005ea
Miguel Carlo Flores <95689181+MFlores01@users.noreply.github.com>
2024-10-12 01:54:41 +0800

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,3 +1,3 @@
 spotify_api_key = "<Your_Spotify_API_Key>"
 openai_api_key = "<Your_OpenAI_API_Key>"
-google_gemini_api_key = "AIzaSyB7-6Ust2YttExAT0Zw63P1Xf2z0Z8v6yk"
+google_gemini_api_key = "____"
